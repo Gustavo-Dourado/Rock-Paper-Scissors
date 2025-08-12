3df409d3a7fba03a15738f2520c35afcc16d526e
Gustavo Dourado <gustavodourado23@gmail.com>
2025-08-12 14:43:05 -0300

    finalized app with UI

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 //getElements
-const initGame = document.querySelector("#play-game");
+const playGameBtn = document.querySelector("#play-game");
 const mainBoard = document.querySelector(".main-board");
 const userBoard = document.querySelector(".user-board");
-const gameDashboard = document.querySelector(".game-dashboard");
-
 const userChoices = document.querySelector(".user-choices");
-
 const initRound = document.querySelector("#play-round");
-
-const nextRound = document.querySelector("#next-round");
-
 const buttonsExit = document.querySelectorAll(".exit");
 const firstExit = buttonsExit[0];
 const secondExit = buttonsExit[1];
-
-const MAX_ROUNDS = 5;
+const gameDashboard = document.querySelector(".game-dashboard");
+const nextRound = document.querySelector("#next-round");
+const controlGameButtons = document.querySelector(".game-dashboard .control-game");
+const finalResult = document.querySelector(".final-result");
+const resetBtn = document.querySelector("#reset-button");
 
 const CHOICES = {
     ROCK: 1, 
@@ -28,7 +25,7 @@ const CHOICES_NAMES = {
     SCISSORS: "scissors",
 }
 
-const NAMES_TRADUZIDOS = {
+const CHOICES_NAMES_PT = {
     [CHOICES.ROCK]: "Pedra",
     [CHOICES.PAPER]: "Papel",
     [CHOICES.SCISSORS]: "Tesoura",
@@ -41,74 +38,123 @@ const ROUND_RESULT = {
 };
 
 // A variável que armazena a escolha do jogador precisa ser global
-let humanChoice = null;
-let gameState;
+let gameState = {
+        round: 1,
+        maxRounds: 5,
+        humanChoice: null,
+        humanScore: 0,
+        botScore: 0,
+    };
 
 // Adiciona event listener para armazenar a escolha do jogador
 userChoices.addEventListener('click', (e) => {
     // Chamamos a função getHumanChoice para validar e pegar o valor
-    humanChoice = getHumanChoice(e);
+    gameState.humanChoice = getHumanChoice(e);
 });
 
 // Adiciona um listener para começar o jogo
-initGame.addEventListener('click', () => {
+playGameBtn.addEventListener('click', () => {
     // Mostra o painel de rodada e inicializa o estado do jogo
     showRoundPanel();
-    gameState = initializeGameState();
-    // Inicia o fluxo do jogo
-    handleRoundStartClick();
+    initNewGame(gameState);
 });
 
-async function handleRoundStartClick() {
-    // Essa promise vai esperar o clique no botão "Jogar" ou "Encerrar"
-    const shouldContinue = await waitForPlayOrExit();
-    
-    if (shouldContinue) {
-        // Se o jogador clicou em "Jogar"
-        if (humanChoice) {
-            console.log('Tudo certo! O jogo pode continuar.');
-            playRound(humanChoice, gameState);
-            showResultsPanel();
+resetBtn.addEventListener('click', showInicialPanel);
 
-            // Await agora para esperar o clique de "Próxima Rodada" ou "Encerrar"
+function initNewGame(gameState){
+    resetGame(gameState); //Reseta variáveis do Jogo
+    handleRoundStartClick(gameState);// Inicia o fluxo principal do jogo
+}
+
+function resetGame(gameState){
+    gameState.round = 1;
+    gameState.maxRounds = 5;
+    gameState.humanChoice = null;
+    gameState.humanScore = 0;
+    gameState.botScore = 0;
+}
+
+async function handleRoundStartClick(gameState) {
+
+    displayRoundCounter(gameState);
+    
+    while(gameState.round <= gameState.maxRounds){
+        // Essa promise vai esperar o clique no botão "Jogar" ou "Encerrar"
+        const shouldContinue = await waitForPlayOrExit();
+        
+        if (!shouldContinue) {
+            encerrarJogo(gameState);
+            return;
+        }
+
+        // Se o jogador clicou em "Jogar"
+        if(!gameState.humanChoice){
+        // Se o jogador não escolheu, esperamos novamente pelo clique 
+            alert('Por favor, escolha Pedra, Papel ou Tesoura antes de jogar!');
+            continue;
+        }
+        
+        playRound(gameState);
+        showResultsPanel();       
+
+        // Await agora para esperar o clique de "Próxima Rodada" ou "Encerrar"
+        if(gameState.round != gameState.maxRounds){ 
             const shouldGoToNextRound = await waitForNextOrExit();
             
-            if (shouldGoToNextRound) {
-                humanChoice = null; // Limpa a escolha para a próxima rodada
-                showRoundPanel();
-                handleRoundStartClick(); // Chama a próxima rodada
-            } else{
-                displayFinalResult(gameState);
-                showInicialPanel();
+            if (!shouldGoToNextRound){
+                encerrarJogo(gameState);
                 return;
             }
-        } else {
-            alert('Por favor, escolha Pedra, Papel ou Tesoura antes de jogar!');
-            // Se o jogador não escolheu, esperamos novamente pelo clique
-            handleRoundStartClick(); 
+            
+            startNextRound( gameState)  
+        } else{
+            finalizeGame(gameState);
         }
-    } else {
-        // Se o jogador clicou em "Encerrar"
-        displayFinalResult(gameState);
-        showInicialPanel();
     }
 }
 
-function initializeGameState(){
-    return {
-        humanScore: 0,
-        botScore: 0,
-    };
+function finalizeGame(gameState){
+     handleDisplayFinalResult(gameState);
+     advanceRound(gameState); 
 }
 
-function playRound(humanChoice, gameState){
+function handleDisplayFinalResult(gameState){
+    //Remover botões de Próximo Round e Encerrar da tela
+    controlGameButtons.classList.add("none");
+
+    const finalMessage = defineFinalMessage(gameState);
+    displayFinalResult(gameState, finalMessage);
+    setTimeout(showFinalResultsPanel, 2000);
+}
+
+function startNextRound(gameState){
+    resetHumanChoice(gameState); // Limpa a escolha para a próxima rodada
+    advanceRound(gameState);
+    displayRoundCounter(gameState);
+    showRoundPanel();
+}
+
+function encerrarJogo(gameState){
+    displayFinalResult(gameState);
+    showInicialPanel();
+}
+
+function resetHumanChoice(gameState){
+    gameState.humanChoice = null;
+}
+
+function advanceRound(gameState){
+    gameState.round++;
+}
+
+function playRound(gameState){
     
     const botChoice = getBotChoice();
-    const roundResult = determineRoundWinner(humanChoice, botChoice);
+    const roundResult = determineRoundWinner(gameState, botChoice);
 
-    updateGameState(gameState, roundResult);
+    updateScore(gameState, roundResult);
 
-    displayRoundResult(humanChoice, botChoice, gameState, 1, roundResult);
+    displayRoundResult(gameState, botChoice, roundResult);
 }
 
 function getHumanChoice(e){
@@ -126,54 +172,56 @@ function getBotChoice(){
     return Math.ceil(Math.random() * 3);
 }
 
-function determineRoundWinner(humanChoice, botChoice){
-    if(humanChoice === botChoice)
+function determineRoundWinner(gameState, botChoice){
+    if(gameState.humanChoice === botChoice)
         return ROUND_RESULT.TIE;
 
-    if (humanChoice.ROCK)
-        return botChoice.SCISSORS ? ROUND_RESULT.HUMAN_WIN : ROUND_RESULT.BOT_WIN;
+    if (gameState.humanChoice === CHOICES.ROCK)
+        return botChoice === CHOICES.SCISSORS ? ROUND_RESULT.HUMAN_WIN : ROUND_RESULT.BOT_WIN;
 
-    if (humanChoice.PAPER)
-        return botChoice.ROCK ? ROUND_RESULT.HUMAN_WIN : ROUND_RESULT.BOT_WIN;
+    if (gameState.humanChoice === CHOICES.PAPER)
+        return botChoice === CHOICES.ROCK ? ROUND_RESULT.HUMAN_WIN : ROUND_RESULT.BOT_WIN;
     
-    if (humanChoice.SCISSORS)
-        return botChoice.PAPER ? ROUND_RESULT.HUMAN_WIN : ROUND_RESULT.BOT_WIN;
+    if (gameState.humanChoice === CHOICES.SCISSORS)
+        return botChoice === CHOICES.PAPER ? ROUND_RESULT.HUMAN_WIN : ROUND_RESULT.BOT_WIN;
 }
 
-function updateGameState(gameState, roundResult){
+function updateScore(gameState, roundResult){
     if(roundResult === ROUND_RESULT.HUMAN_WIN)
         gameState.humanScore++;
     else if(roundResult === ROUND_RESULT.BOT_WIN)
         gameState.botScore++;
 }
 
-function displayRoundResult(humanChoice, botChoice, gameState, round, roundResult){
+function displayRoundResult(gameState, botChoice, roundResult){
 
-    const roundMessage = createRoundMessage(humanChoice, botChoice, roundResult);
+    const roundMessage = createRoundMessage(gameState, botChoice, roundResult);
 
-        document.querySelector("#round-indicator span").textContent = round;
-        document.querySelector("#human-choice-name").textContent = NAMES_TRADUZIDOS[humanChoice];
-        document.querySelector("#bot-choice-name").textContent =  CHOICE_NAMES[botChoice];
+        document.querySelector("#human-choice-name").textContent = CHOICES_NAMES_PT[gameState.humanChoice];
+        document.querySelector("#bot-choice-name").textContent =  CHOICES_NAMES_PT[botChoice];
         document.querySelector("#round-message-winner").textContent = roundMessage;
         document.querySelector("#human-score").textContent = gameState.humanScore;
         document.querySelector("#bot-score").textContent = gameState.botScore;
-
-        document.querySelector("#round-counter #round-actual").textContent = round;
-        document.querySelector("#round-counter #round-total").textContent = MAX_ROUNDS;
 }
 
-function createRoundMessage(humanChoice, botChoice, roundResult){
+function createRoundMessage(gameState, botChoice, roundResult){
     switch(roundResult){
         case ROUND_RESULT.HUMAN_WIN:
-            return `Você VENCEU nesta rodada! ${CHOICE_NAMES[humanChoice]} ganha de ${CHOICE_NAMES[botChoice]}`;
+            return `Você VENCEU nesta rodada! ${CHOICES_NAMES_PT[gameState.humanChoice]} ganha de ${CHOICES_NAMES_PT[botChoice]}`;
         case ROUND_RESULT.BOT_WIN:
-            return `Você PERDEU nesta rodada! ${CHOICE_NAMES[botChoice]} ganha de ${CHOICE_NAMES[humanChoice]}`;
+            return `Você PERDEU nesta rodada! ${CHOICES_NAMES_PT[botChoice]} ganha de ${CHOICES_NAMES_PT[gameState.humanChoice]}`;
         case ROUND_RESULT.TIE:
             return "Houve EMPATE"; 
     }
 }
 
-function displayFinalResult(gameState){
+function displayFinalResult(gameState, finalMessage){
+    document.querySelector(".display #final-message-winner").textContent = finalMessage;
+    document.querySelector(".display #final-human-score").textContent = gameState.humanScore;
+    document.querySelector(".display #final-bot-score").textContent = gameState.botScore;
+}
+
+function defineFinalMessage(gameState){
     const {humanScore, botScore} = gameState;
     let message;
     
@@ -184,40 +232,41 @@ function displayFinalResult(gameState){
     else
         message = "JOGO EQUILIBRADO, Houve EMPATE!";
 
-    message += `\n \t Seu Score: ${humanScore} \n \t Computador Score: ${botScore}`;
-    alert(message);
+    return message;
 }
 
 //funções para modificar displays do jogo
 function showInicialPanel(){
-    mainBoard.classList.remove('block');
+    playGameBtn.classList.remove('none');
     mainBoard.classList.add('none');
-
-    initGame.classList.remove('none');
-    
-    gameDashboard.classList.remove('flex');
     gameDashboard.classList.add('none');
+    finalResult.classList.add("none");
 }
 
 function showRoundPanel(){
-    initGame.classList.add('none');
+    playGameBtn.classList.add('none');
 
     mainBoard.classList.remove('none');
-    mainBoard.classList.add('block');
-   
     userBoard.classList.remove('none');
-    userBoard.classList.add('flex');
 
-    gameDashboard.classList.remove('flex');
     gameDashboard.classList.add('none');
 }
 
 function showResultsPanel(){
-    userBoard.classList.remove('flex');
     userBoard.classList.add('none');
-
     gameDashboard.classList.remove('none');
-    gameDashboard.classList.add('flex');
+    controlGameButtons.classList.remove("none");
+}
+
+function showFinalResultsPanel(){
+    mainBoard.classList.add("none");
+    finalResult.classList.remove("none");
+}
+
+function displayRoundCounter(gameState){
+    document.querySelector("#round-indicator span").textContent = gameState.round;
+    document.querySelector("#round-counter #round-actual").textContent = gameState.round;
+    document.querySelector("#round-counter #round-total").textContent = gameState.maxRounds;
 }
 
 async function waitForPlayOrExit(){
@@ -243,10 +292,11 @@ async function waitForPlayOrExit(){
 async function waitForNextOrExit(){
     return new Promise((resolve) => {
 
-        nextRound.removeEventListener('click', handleNextOrExit);
-        secondExit.removeEventListener('click', handleNextOrExit);
-
         function handleNextOrExit(e){
+
+            nextRound.removeEventListener('click', handleNextOrExit);
+            secondExit.removeEventListener('click', handleNextOrExit);
+            
             if(e.target.id.includes("next")){
                 resolve(true);
             } else if(e.target.id.includes("exit")){
